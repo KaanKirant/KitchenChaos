912d7a23b305b320917cc5853050a2c6f6acff55
Kaan Kirant <kaankirant98@gmail.com>
2025-10-16 10:24:37 +0300

    Plate Complete Visual

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,8 +1,16 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs
+    {
+        public KitchenObjectSO kitchenObjectSO;
+    }
+
+
     [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
     private List<KitchenObjectSO> kitchenObjectSOList;
 
@@ -10,6 +18,7 @@ public class PlateKitchenObject : KitchenObject
     {
         kitchenObjectSOList = new List<KitchenObjectSO>();
     }
+
     public bool TryAddIngredient(KitchenObjectSO kitchenObjectSO)
     {
         if(!validKitchenObjectSOList.Contains(kitchenObjectSO)) return false; // Not a valid ingredient.
@@ -18,6 +27,11 @@ public class PlateKitchenObject : KitchenObject
         else
         {
             kitchenObjectSOList.Add(kitchenObjectSO);
+
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs
+            {
+                kitchenObjectSO = kitchenObjectSO
+            });
             return true;
         }
     }
